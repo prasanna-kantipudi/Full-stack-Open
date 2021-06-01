75b9231438dc2be87891982e8da239269e552679
prasanna-kantipudi <74001562+prasanna-kantipudi@users.noreply.github.com>
2021-06-02 00:16:29 +0530

    Add files via upload

---
+++ part2/2.2/App.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id:1,
+    parts: [
+      {
+        id:1,
+       name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id:2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id:3,
+        name: 'State of a component',
+        exercises: 14
+      },
+      {
+        id:4,
+        name: 'redux',
+        exercises: 11
+      }
+    ]
+  }
+  
+
+  return (
+    <div>
+     <Course p={course}/>
+    </div>
+  )
+}
+const Course=({p})=>{
+  return(
+    <div>
+    <Header k={p.name}/>
+    <Content b={p.parts}/>
+    <Total b={p.parts}/>
+
+    </div>
+  )
+}
+
+const Header=({k})=>{
+  return(
+    <h2>{k}</h2>
+  )
+}
+const Content=({b})=>{
+  return(
+    <div>
+      {
+        b.map(r=>{
+          return(
+            <Part key={r.id} a={r.name} v={r.exercises}/>
+          )
+        }
+
+        )
+      }
+    </div>
+  )
+}
+const Part=({a,v})=>{
+  return(
+    <p>{a} {v}</p>
+  )
+}
+const Total=({b})=>{
+  let x=0
+  for( const j of b)
+  {
+    x+=j.exercises
+  }
+   return(
+     <p>total of {x} execrises</p>
+   )
+}
+export default App
